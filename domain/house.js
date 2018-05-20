2647890747cb643ff44eff3689d8a3e8a98fa944
MisterArdavan <theapollobelenus-gitlab@yahoo.com>
2018-05-20 18:02:01 +0430

    Added user to domain

--- domain/house.js
+++ domain/house.js
@@ -1,14 +1,33 @@
-const House = require('../models').House;
-const functions = {
-    async createHouse () {
-        const user = await House.create({username: 'ali', email: 'a@b.c', password: '123'});
-        console.log(user);
-        return user;
-    },
-    async getAllUsers () {
-        const users = await User.findAll({});
-        return users;
-    }
-};
+const HouseModel = require('../models').House;
 
-module.exports = functions;
+class House {
+    constructor(id, buildingType, address, imageURL, phone,
+                description, expireTime, area, basePrice, rentPrice, sellPrice, dealType) {
+        this._id = id;
+        this._buildingType = buildingType;
+        this._address = address;
+        this._imageURL = imageURL;
+        this._phone = phone;
+        this._description = description;
+        this._expireTime = expireTime;
+        this._area = area;
+        this._basePrice = basePrice;
+        this._rentPrice = rentPrice;
+        this._sellPrice = sellPrice;
+        this._dealType = dealType;
+    }
+}
+
+// const functions = {
+//     async createHouse () {
+//         const user = await House.create({username: 'ali', email: 'a@b.c', password: '123'});
+//         console.log(user);
+//         return user;
+//     },
+//     async getAllUsers () {
+//         const users = await User.findAll({});
+//         return users;
+//     }
+// };
+
+module.exports = House;
---
+++ domain/user.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(name, isAdmin) {
+        this._name = name;
+        this._isAdmin = isAdmin;
+    }
+
+    get name() {
+        return this._name
+    }
+
+    get isAdmin() {
+        return this._isAdmin
+    }
+}
