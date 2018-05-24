813901cc60fb329c0fb25322bcb538f2ef35ebf9
nickybayat <nickybayat95@gmail.com>
2018-05-24 15:03:35 +0430

    working on search

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const Individual = require('./domain/individual');
+const Search =require('./domain/search');
 const individual = new Individual("بهنام همایون","021123456",0,'behnamhomayoon','password',false);
 
 const port = process.env.port || 8080;
@@ -33,6 +34,10 @@ app.use(function(req, res, next) {
 
 app.use('/public', express.static(__dirname + '/statics'));
 
+app.get('/', function (req,res) {
+    res.send('Welcome to Khanebedoosh');
+})
+
 app.post('/balance', function(req, res) {
     let value = req.body.balance;
     try{
@@ -50,7 +55,23 @@ app.post('/balance', function(req, res) {
     }
 });
 
+app.get('/houses',function (req,res) {
+    let minArea = req.query.minArea;
+    let buildingType = req.query.buildingType;
+    let dealType = req.query.dealType;
+    let maxPrice = req.query.maxPrice;
+    try{
+        var searchHouses = new Search(minArea,buildingType,dealType,maxPrice);
+        let requestedHouses = searchHouses.getRequestedHousesFromAllUsers(searchHouses);
+        // requestedHouses must be a json array
+        var result = {'houses' : requestedHouses};
+        res.status(200).json(result);
 
+    }
+    catch {
+        res.status(400).json({'msg': "Error in finding houses! Try Again!"});
+    }
+})
 
 app.listen(port);
 console.log('Listening on port: ' + port);
